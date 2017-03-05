58ce2dfe301501c11ed29f385e3481542ce4422a
AgtLucas <lucas@agtlucas.com>
2017-03-05 08:09:18 -0300

    Update sample

--- src/app.js
+++ src/app.js
@@ -2,13 +2,22 @@ import xs from 'xstream'
 import { html } from 'snabbdom-jsx'
 
 export function App (sources) {
-  const vtree$ = xs.of(
-    <div className='container'>
-      <p className='text'>OK!</p>
-    </div>
-  )
+  // const vtree$ = xs.of(
+  //   <div className='container'>
+  //     <p className='text'>OK!</p>
+  //   </div>
+  // )
+
   const sinks = {
-    DOM: vtree$
+    DOM: sources.DOM.select('input').events('click')
+      .map(ev => ev.target.checked)
+      .startWith(false)
+      .map(toggled =>
+        <div>
+          <input type='checkbox' /> Toggle me
+          <p>{toggled ? 'ON' : 'OFF'}</p>
+        </div>
+      )
   }
   return sinks
 }
